bb6b03cfa87268ed725c507167ba7e01626bdf2f
DinaOluf <91533917+DinaOluf@users.noreply.github.com>
2022-06-02 16:34:42 +0200

    Minor adjustments

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
 
 const profile = document.querySelector(".profile");
-const card1 = document.getElementById("card-1");
-const card2 = document.getElementById("card-2");
-const card3 = document.getElementById("card-3");
-
 const projectCard = document.querySelectorAll(".project-card");
 
 const circle1 = document.querySelector(".circle-1");
@@ -12,28 +8,22 @@ const circle2 = document.querySelector(".circle-2");
 profile.addEventListener("mouseenter", function() {
     profile.style.animation = "0.2s forwards 1 maximize";
     circle1.style.animation = "0.2s forwards 1 maximize";
-    circle1.firstChild.style.animation = "0.3s forwards 1 maximize";
 });
 
 profile.addEventListener("mouseleave", function() {
     profile.style.animation = "0.2s forwards 1 minimize";
     circle1.style.animation = "0.2s forwards 1 minimize";
-    circle1.firstChild.style.animation = "0.2s forwards 1 minimize";
 });
-
-console.log(projectCard);
 
 projectCard.forEach(function(card) {
     card.addEventListener("mouseenter", function(e){
         e.target.style.animation = "0.2s forwards 1 maximize";
         circle2.style.animation = "0.2s forwards 1 maximize";
-        circle2.firstChild.style.animation = "0.2s forwards 1 maximize";
     });
 
     card.addEventListener("mouseleave", function(e){
         e.target.style.animation = "0.2s forwards 1 minimize";
         circle2.style.animation = "0.2s forwards 1 minimize";
-        circle2.firstChild.style.animation = "0.2s forwards 1 minimize";
     });
 });
 
