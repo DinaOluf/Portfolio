944c8f039e42c327ae194fcc6089b35c4cd44eef
Dina <dina.olufsen@hotmail.com>
2023-06-02 15:15:29 +0200

    Finished Hop Game

--- js/hop.js
+++ js/hop.js
@@ -1,5 +1,6 @@
 const hopGame = document.getElementById("hopGame")
 const hopper = document.getElementById("hopper");
+const hopperImg = document.getElementById("hopperImg");
 const rock1 = document.getElementById("rock1");
 const rock2 = document.getElementById("rock2");
 const rock3 = document.getElementById("rock3");
@@ -9,6 +10,8 @@ const hiScore = document.getElementById("hiScore");
 const localHiScore = localStorage.getItem("hiScore");
 
 let spawner;
+let isPaused = true;
+let timeScore = 0;
 
 if(localHiScore > hiScore.innerHTML){
     hiScore.innerHTML = localHiScore;
@@ -18,109 +21,118 @@ if(overlay.className === "show-overlay"){
     document.addEventListener("keydown", function (e) {
         overlay.className = "hide-overlay"; 
         hopGame.style.animationPlayState = 'running';
-        handleIntervals();
+        isPaused = false;
     });
 }
 
-function handleIntervals() {
-    // Will execute rockSpawner every 2 seconds 
-    clearInterval(spawner);
-    if(overlay.className === "hide-overlay"){
-        spawner = setInterval(rockSpawner, 2000);
-    }
-}
+const rockSpawner = setInterval(function()  {
+    if(!isPaused){
+        let number = random([1, 2, 3]);
+       
+        if(number === 1 && rock1.className !== "slide"){
+           rock1.className = "slide";
+           rock1.style.animationPlayState = "running";
+           setTimeout(() => {
+               rock1.classList.remove("slide");
+           }, 3000)
+        }
+       
+        if(number === 2 && rock2.className !== "slide"){
+           rock2.className = "slide";
+           rock2.style.animationPlayState = "running";
+           setTimeout(() => {
+               rock2.classList.remove("slide");
+           }, 3000)
+        }
+       
+        if(number === 3 && rock3.className !== "slide"){
+           rock3.className = "slide";
+           rock3.style.animationPlayState = "running";
+           setTimeout(() => {
+               rock3.classList.remove("slide");
+           }, 3000)
+        }
+    } 
+}, 2000);
 
-function rockSpawner() {
-    let number = random([1, 2, 3]);
-
-    console.log(number)
-   
-    if(number === 1 && rock1.className !== "slide"){
-       rock1.className = "slide";
-       rock1.style.animationPlayState = "running";
-       setTimeout(() => {
-           rock1.classList.remove("slide");
-       }, 3000)
+const setScore = setInterval(function() {
+    if(!isPaused) {
+        timeScore++;
+        score.innerHTML = timeScore;
     }
-   
-    if(number === 2 && rock2.className !== "slide"){
-       rock2.className = "slide";
-       rock2.style.animationPlayState = "running";
-       setTimeout(() => {
-           rock2.classList.remove("slide");
-       }, 3000)
-    }
-   
-    if(number === 3 && rock3.className !== "slide"){
-       rock3.className = "slide";
-       rock3.style.animationPlayState = "running";
-       setTimeout(() => {
-           rock3.classList.remove("slide");
-       }, 3000)
-    }
-}
+}, 100);
 
 function random(numbers) {
     return numbers[Math.floor(Math.random()*numbers.length)];
 }
 
-function showOverlay() {
+function pauseGame() {
+    isPaused = true;
     overlay.className = "show-overlay";
-    overlay.innerHTML = "<div>THE END!</div><div>Press any key to start again</div>";
-    handleIntervals();
-    rock1.style.animationPlayState = "paused";
-    rock1.className.remove = "slide";
-    rock2.style.animationPlayState = "paused";
-    rock2.className.remove = "slide";
-    rock3.style.animationPlayState = "paused";
-    rock3.className.remove = "slide";
+    overlay.innerHTML = `<div>GAME OVER!</div><div>Score: ${score.innerHTML}</div><div>Press any key to start again</div>`;
     hopGame.style.animationPlayState = 'paused';
+    rock1.style.animationPlayState = "paused";
+    rock2.style.animationPlayState = "paused";
+    rock3.style.animationPlayState = "paused";
+    rock1.className.remove = "slide";
+    rock2.className.remove = "slide";
+    rock3.className.remove = "slide";
 
-    if(hiScore.innerHTML < score.innerHTML) {
+    if(Number(hiScore.innerHTML) < Number(score.innerHTML)) {
         hiScore.innerHTML = score.innerHTML;
+    } 
+    if (Number(hiScore.innerHTML) > Number(localHiScore)) {
         localStorage.setItem("hiScore", hiScore.innerHTML);
     }
+
+    timeScore = 0;
 }
 
 
 function jump() {
     if (hopper.classList != "jump") {
     hopper.classList.add("jump");
+    hopperImg.src = "../images/hop/hopper2.png";
 
     setTimeout(function () {
         hopper.classList.remove("jump");
+        hopperImg.src = "../images/hop/hopper1.png";
     }, 1000);
     }
 }
 
-setInterval(function () {
-    // get current hopper Y position
+const watchCollision = setInterval(function () {
     let hopperBottom = parseInt(window.getComputedStyle(hopper).getPropertyValue("bottom")); 
-
-    // get current rock X position
     let rock1Left = parseInt(window.getComputedStyle(rock1).getPropertyValue("left"));
     let rock2Left = parseInt(window.getComputedStyle(rock2).getPropertyValue("left"));
     let rock3Left = parseInt(window.getComputedStyle(rock3).getPropertyValue("left"));
 
-//   console.log(hopperBottom);
-//   console.log(rock1Left);
-
-//   // detect collision
-    if (rock1Left < 140 && rock1Left > 80 && hopperBottom <= 100) {
-        showOverlay();
+    if (rock1Left < 140 && rock1Left > 100 && hopperBottom <= 90) {
+        isPaused = true;
+        pauseGame();
     }
 
-    if (rock2Left < 140 && rock1Left > 80 && hopperBottom <= 80) {
-        showOverlay();
+    if (rock2Left < 130 && rock1Left > 100 && hopperBottom <= 70) {
+        isPaused = true;
+        pauseGame();
     }
 
-    if (rock3Left < 140 && rock1Left > 80 && hopperBottom <= 110) {
-        showOverlay();
+    if (rock3Left < 150 && rock1Left > 100 && hopperBottom <= 100) {
+        isPaused = true;
+        pauseGame();
     }
 }, 10);
+
+const runningAnimation = setInterval(function () {
+    if(!isPaused && hopper.className !== "jump"){
+        hopperImg.src = "../images/hop/hopper2.png";
+        setTimeout(() => {
+            hopperImg.src = "../images/hop/hopper1.png";
+        }, 500)
+    }
+}, 1000)
 
 
 document.addEventListener("keydown", function (e) {
     jump();
-    handleIntervals();
 });
